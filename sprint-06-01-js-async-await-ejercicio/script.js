f94e41ee0253f1a3ebd0171a6436f40e3b7211f3
bernat alcudia <bernatalcudia@gmail.com>
2025-02-26 11:38:45 +0100

    added try and catch to catch errors

--- sprint-06-01-js-async-await-ejercicio/script.js
+++ sprint-06-01-js-async-await-ejercicio/script.js
@@ -1,29 +1,48 @@
 //Asyn/Await
 
 async function requestNameEmailAddress(link) {
-    const result = await fetch(link);
-    let data = await result.json();
-    let { city } = data.address;
-    data = data.name + data.email + city;
-    console.log(data);
+    try {
+        const result = await fetch(link);
+        let data = await result.json();
+        let { city } = data.address;
+        data = data.name + data.email + city;
+        console.log(data);
+    } catch (error) {
+        throw new Error(error);
+    }
 }
 
 const link1 = "https://jsonplaceholder.typicode.com/users/1";
 const link2 = "https://jsonplaceholder.typicode.com/users/2";
 const link3 = "https://jsonplaceholder.typicode.com/users/3"
 
-const promise1 = fetch("https://jsonplaceholder.typicode.com/users/1")
-    .then(result => (result.json()))
-    .then(data => (console.log(data.name, data.email, data.address)))
+try {
+    const promise1 = fetch("https://jsonplaceholder.typicode.com/users/1")
+        .then(result => (result.json()))
+        .then(data => (console.log(data.name, data.email, data.address)))
+        .catch(error => { throw new Error(error) });
+} catch (error) {
+    throw new Error(error);
+}
 
-const promise2 = fetch("https://jsonplaceholder.typicode.com/users/2")
-    .then(result => (result.json()))
-    .then(data => (console.log(data.name, data.email, data.address)))
+try {
+    const promise2 = fetch("https://jsonplaceholder.typicode.com/users/2")
+        .then(result => (result.json()))
+        .then(data => (console.log(data.name, data.email, data.address)))
+        .catch(error => { throw new Error(error) });
+} catch (error) {
+    throw new Error(error);
+}
 
+try {
+    const promise3 = fetch("https://jsonplaceholder.typicode.com/users/3")
+        .then(result => (result.json()))
+        .then(data => (console.log(data.name, data.email, data.address)))
+        .catch(error => { throw new Error(error) });
+} catch (error) {
+    throw new Error(error);
+}
 
-const promise3 = fetch("https://jsonplaceholder.typicode.com/users/3")
-    .then(result => (result.json()))
-    .then(data => (console.log(data.name, data.email, data.address)))
 
 
 
